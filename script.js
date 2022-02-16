f5e54dcb92b2428b685066c8227887934ba41fba
Yasir1986 <yasir.akbar86@hotmail.com>
2022-02-16 19:23:21 +0000

    mouse event reverted

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ function handleLose() {
   computerPaddle.reset()
 }
 
-document.addEventListener("ontouchmove", e => {
+document.addEventListener("mousemove", e => {
   playerPaddle.position = (e.y / window.innerHeight) * 100
 })
 
